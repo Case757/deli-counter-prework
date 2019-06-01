59652a8cd5760655dc869f44ce7606483f491937
David Fellows <dfellows12@gmail.com>
2019-06-01 02:25:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(line)
     puts "The line is currently empty."
   else
     line.each_with_index do |name, index|
-    myLine.push("#{index}. #{name} ")  
+    myLine.push("#{index + 1}. #{name} ")  
   end
   puts "The line is currently: #{myLine}"
 end
